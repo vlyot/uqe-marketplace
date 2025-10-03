6be60d0c8a64f3d071a09422a3d2e4aa880d0c24
Kai Chong Ng <ngkaichong10@gmail.com>
2025-10-04 01:49:46 +0800

    package changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE || '/',
+  base: '/uqe-marketplace/',
   build: { sourcemap: true }
 })
